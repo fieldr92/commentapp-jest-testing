467913892b6895c6aff5d53e092d291278a24b43
fieldr <fieldr92@gmail.com>
2019-07-11 09:43:37 +0100

    Add auth conditional render

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-export default () => {
-  return (
-    <div>
-      <Route path="/posts" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>;
+    return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>;
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Post a Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/posts" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
 };
+
+export default connect(
+  mapStateToProps,
+  actions
+)(App);
